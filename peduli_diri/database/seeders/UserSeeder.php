dd8f6c04d63e2c7bd5ecfc65e2da2aead0d7b551
Arbiiim <arbiiim26@gmail.com>
2022-07-29 09:00:29 +0700

    Tampil Data

---
+++ peduli_diri/database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory()->create([
+            'name' =>'Vestia Zeta',
+            'email' =>'zeta@gmail.com',
+            'password' =>bcrypt('12345'),
+            'remember_token' =>Str::random('60'),
+        ]);
+    }
+}
